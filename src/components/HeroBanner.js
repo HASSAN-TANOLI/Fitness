f5c534f9b7e212033c80cce6185e5d016b77cf0b
HASSAN-TANOLI <hassantanoli309@gmail.com>
2022-10-31 14:06:05 +0500

    last commit

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -13,7 +13,7 @@ const HeroBanner = () => {
       p="20px"
     >
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
-        Fitness Club
+        Fitness Spot
       </Typography>
 
       <Typography
